73d989d0aecd675fb098cdd121f5de1e72388f9f
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2026-01-14 23:16:26 +0530

    contracts: update: dependencies + config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -4,19 +4,46 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
+const getPrivateKey = () => {
+  const pk = process.env.ACCOUNT_PRIVATE_KEY;
+  if (!pk) throw new Error("ACCOUNT_PRIVATE_KEY not found");
+  return pk;
+};
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   defaultNetwork: "mantleSepolia",
   networks: {
     mantle: {
       url: "https://rpc.mantle.xyz", // mainnet
-      accounts: [process.env.ACCOUNT_PRIVATE_KEY ?? ""],
+      accounts: [getPrivateKey()],
     },
     mantleSepolia: {
       url: "https://rpc.sepolia.mantle.xyz",
-      accounts: [process.env.ACCOUNT_PRIVATE_KEY ?? ""],
+      accounts: [getPrivateKey()],
     },
-  }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+    customChains: [
+      {
+        network: "mantleSepolia",
+        chainId: 5003,
+        urls: {
+          apiURL: "https://api-sepolia.mantlescan.xyz/api",
+          browserURL: "https://sepolia.mantlescan.xyz/",
+        },
+      },
+      {
+        network: "mantle",
+        chainId: 5000,
+        urls: {
+          apiURL: "https://api.mantlescan.xyz/api",
+          browserURL: "https://mantlescan.xyz",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
